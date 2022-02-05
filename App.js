193e5cdde2c3b2743087617ac65fd556e74ab834
krishkharal12345 <krishkharal190@gmail.com>
2022-02-05 13:16:30 +0545

    changes file

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { View,Image } from "react-native";
+import { View, Text } from "react-native";
 import React, { Component } from "react";
 import AwesomeCardWithYoutubeLink from "./components/AwesomeCardWithYoutubeLink";
 
 export class App extends Component {
   render() {
     return (
+      <View>
+        <Text>hello world</Text>
         <AwesomeCardWithYoutubeLink />
+      </View>
     );
   }
 }
